b8f326d09d3062b88a1ad4d084f7680055bc7686
taddeimania <jtaddei@gmail.com>
2017-10-05 12:54:00 -0400

    made react components more fully features

--- static/js/app/src/components/MyPeople.jsx
+++ static/js/app/src/components/MyPeople.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const MyPeople = ({users}) =>
-    <div>
-        <h1>Look at these cool people</h1>
-
-        <ul>
-            {users.map(user =>
-            <li key={user.id}>
-                <a href={`/user/${user.id}/`}>
-                    {user.username}
-                </a>
-            </li>
-            )}
-
-        </ul>
-    </div>
-
-
+class MyPeople extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Look at these cool people</h1>
+                <ul>
+                    {this.props.users.map(user =>
+                    <li key={user.id}>
+                        <a href={`/user/${user.id}/`}>
+                            {user.username}
+                        </a>
+                    </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    React.createElement(MyPeople, window.props),
+    <MyPeople {...window.props} />,
     window.react_mount,
 )
--- static/js/app/src/components/UserDetail.jsx
+++ static/js/app/src/components/UserDetail.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const UserDetail = ({user}) =>
-    <div>
-        <h1>Look at this cool person</h1>
-        <div>
-            {user.username}
-        </div>
-    </div>
-
+class UserDetail extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Look at this cool person</h1>
+                <div>
+                    {this.props.user.username}
+                </div>
+            </div>
+        )
+    }
+}
 
 ReactDOM.render(
-    React.createElement(UserDetail, window.props),
+    <UserDetail {...window.props} />,
     window.react_mount,
 )
